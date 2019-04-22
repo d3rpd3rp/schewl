b3559c9aac0c8137412003c24a43607ce33236b2
d3rpd3rp <d3rpd3rp@users.noreply.github.com>
2019-04-21 20:22:23 -0400

    started with google news api and sample python sdk

---
+++ sparkML/python-routines/april-2019-news.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3.6
+import newsapi
+import json
+
+apiO = newsapi.NewsApiClient(api_key='54d8cbfd13474052801605a1f0412c2d')
+
+sourceList = 'bbc-news,the-verge,abc-news,crypto coins news,ary news,associated press,\
+    wired,aftenposten,australian financial review,axios,\
+	bbc news,bild,blasting news,bloomberg,business insider,google news,\
+	hacker news,info money,the next web,the verge'
+
+pages = {}
+pageNo = 1
+
+while True:
+    page = apiO.get_everything(language = 'en', from_param = '2019-03-23', \
+        sort_by = 'popularity', sources = sourceList, page = pageNo)
+    print(page, type(page))
+    if not page:
+        break
+    else:
+        pages[pageNo] = page
+        pageNo += 1
